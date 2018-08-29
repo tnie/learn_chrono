bcb7a85f06a8f765fc1b7f4e0708525f72e390ad
nielyd <nielong2014@gmail.com>
2018-08-29 18:05:23 +0800

    clean "inline"

--- SafeQueue/SafeQueue.h
+++ SafeQueue/SafeQueue.h
@@ -43,7 +43,7 @@ private:
 
 // Add an element to the queue.
 template<class T>
-inline void threadsafe_queue<T>::push(const T & t)
+void threadsafe_queue<T>::push(const T & t)
 {
     std::lock_guard<std::mutex> lock(m);
     q.push(t);
@@ -53,7 +53,7 @@ inline void threadsafe_queue<T>::push(const T & t)
 // Get the "front"-element.
 // If the queue is empty, wait till a element is avaiable.
 template<class T>
-inline void threadsafe_queue<T>::wait_and_pop(T & value)
+void threadsafe_queue<T>::wait_and_pop(T & value)
 {
     std::unique_lock < std:; mutex > lk(m);
     c.wait(lk, [this]() {return q.empty() == false; });
@@ -62,7 +62,7 @@ inline void threadsafe_queue<T>::wait_and_pop(T & value)
 }
 
 template<class T>
-inline std::shared_ptr<T> threadsafe_queue<T>::wait_and_pop()
+std::shared_ptr<T> threadsafe_queue<T>::wait_and_pop()
 {
     std::unique_lock<std::mutex> lock(m);
     // release lock as long as the wait and reaquire it afterwards.
@@ -75,7 +75,7 @@ inline std::shared_ptr<T> threadsafe_queue<T>::wait_and_pop()
 
 //
 template<class T>
-inline bool threadsafe_queue<T>::try_pop(T & value)
+bool threadsafe_queue<T>::try_pop(T & value)
 {
     std::lock_guard<std::mutex> lock(m);
     if (q.empty())
@@ -89,7 +89,7 @@ inline bool threadsafe_queue<T>::try_pop(T & value)
 }
 
 template<class T>
-inline std::shared_ptr<T> threadsafe_queue<T>::try_pop()
+std::shared_ptr<T> threadsafe_queue<T>::try_pop()
 {
     std::lock_guard<std::mutex> lock(m);
     if (q.empty())
@@ -103,7 +103,7 @@ inline std::shared_ptr<T> threadsafe_queue<T>::try_pop()
 
 //
 template<class T>
-inline bool threadsafe_queue<T>::empty() const
+bool threadsafe_queue<T>::empty() const
 {
     std::lock_guard<std::mutex> lock(m);
     return q.empty();
