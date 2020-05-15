d1beb2f347c6707a7315b7cd3b2fa55b4a3a31c3
nielyd <niel@yd.com>
2020-05-15 16:33:21 +0800

    he bool-returning version of await_suspend() can be useful

--- ConsoleApplication2/addawaiter.h
+++ ConsoleApplication2/addawaiter.h
@@ -12,12 +12,15 @@ struct Add100Awaiter
     {
 
     }
+
+#ifdef _ANOTHER__
     // 如果结果已经得到，不需要执行协程
     bool await_ready()
     {
         result_ = Add100Random(num_);
         return (result_!= nullptr);
     }
+    // 返回类型为空的函数：执行完毕后直接让出执行
     void await_suspend(std::experimental::coroutine_handle<> h)
     {
         Add100ByCallback(num_,
@@ -27,6 +30,26 @@ struct Add100Awaiter
             h.resume();
         });
     }
+#else
+    bool await_ready()
+    {
+        return false;
+    }
+    // the awaiter might start an async operation that
+    // can sometimes complete synchronously.
+    bool await_suspend(std::experimental::coroutine_handle<> h)
+    {
+        if (result_ = Add100Random(num_))
+            return false;
+        Add100ByCallback(num_,
+            [h, this](int value)
+        {
+            result_ = std::make_shared<int>(value);
+            h.resume();
+        });
+        return true;
+    }
+#endif
     int await_resume()
     {
         if (result_)
