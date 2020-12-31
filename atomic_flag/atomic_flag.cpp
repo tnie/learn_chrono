54724470b04d65e5a8a87f3d3905f50d653ad72d
niel <nielong@yd.com>
2020-12-31 15:36:04 +0800

    使用自旋锁避免输出混乱

--- atomic_flag/atomic_flag.cpp
+++ atomic_flag/atomic_flag.cpp
@@ -7,21 +7,21 @@
 
 using namespace std;
 
-mutex mt;
+//std::atomic_flag 是原子布尔类型。不同于所有 std::atomic 的特化，它保证是免锁的。
+// 和 typedef atomic<bool> atomic_bool; 不同的
 atomic_flag lock_cout = ATOMIC_FLAG_INIT;
 
 void append_number(int x)
 {
-    // 方式①
-    //lock_guard<mutex> lg(mt);
-    // 方式②，不好用
-    while (lock_cout.test_and_set())
+    for (size_t i = 0; i < 10; i++)
     {
-        // 不 sleep ，死循环非常坑
-        this_thread::sleep_for(1ms);
+        // 自旋锁。不使用自旋锁或互斥锁，打印存在竞争，输出混乱
+        while (lock_cout.test_and_set())
+            ;   // spin
+        cout << "Output form " << "threadd #" << x << "\n";
+        lock_cout.clear();
     }
-    cout << "threadd #" << x << endl;
-    lock_cout.clear();
+
 }
 
 int main()
