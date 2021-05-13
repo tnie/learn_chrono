bf2a05b26fb6691657ad56b19ab6ee3eb1ea7d0e
niel <nielong@yd.com>
2021-05-13 16:35:14 +0800

    最简单的 awaiter + 最简单的 promise = 最简单的协程

---
+++ coro/REAMME.md
@@ -0,0 +1,5 @@
+https://devblogs.microsoft.com/oldnewthing/20191209-00/?p=103195
+
+> Therefore, it is important that your awaiter not use its this pointer once it has arranged for the handle to be invoked somehow, because the this pointer may no longer be valid.
+
+最简单的 awaiter + 最简单的 promise = 最简单的协程
--- coro/test.cpp
+++ coro/test.cpp
@@ -17,8 +17,49 @@ Task Add100Coroutine(int a)
     co_return;
 }
 
+//参考 https://devblogs.microsoft.com/oldnewthing/20191209-00/?p=103195
+struct awaiter : public experimental::suspend_always
+{
+    const std::string important = "Therefore, it is important that your awaiter not use its this pointer \
+once it has arranged for the handle to be invoked somehow, \
+because the this pointer may no longer be valid.";
+    ~awaiter()
+    {
+        spdlog::warn("awaiter destructor.");
+    }
+    void await_suspend(experimental::coroutine_handle<> handle)
+    {
+        spdlog::info("the handle to be invoked ");
+        handle.resume();
+        spdlog::info("before the await_suspend finishes running.");
+        if (false)
+        {
+            //Therefore, it is important that your awaiter not use its this pointer
+            //once it has arranged for the handle to be invoked somehow,
+            //because the this pointer may no longer be valid.
+            spdlog::error(this->important);
+        }
+    }
+};
+
+// 协程的返回值，用于和协程交互（比如恢复执行）
+// 为了简单，协程的返回值只实现要求的接口，但什么功能都不做。见 Task 定义
+Task simplest_coro()
+{
+    // 这个不是协程自行恢复，而是根本就没有暂停
+    co_await experimental::suspend_never();
+    // 这个暂停了无法恢复
+    //co_await experimental::suspend_always();
+
+    co_await awaiter();
+    spdlog::info("The entire function have run to completion!");
+
+}
+
 int main()
 {
+    simplest_coro();
+    return 0;
     for (size_t i = 0; i < 1; i++)
     {
         Add100Coroutine(i);
